b482a12dbeecf555c7de89717cf11336ec211399
Adam Borowski <kilobyte@angband.pl>
2022-05-29 14:29:59 +0200

    Fix build failure with no hyperscan.

--- substitute.c
+++ substitute.c
@@ -78,7 +78,9 @@ static void parse_sub(const char *left_, const char *right,  bool gag, struct se
     }
     kb_put(trip, sub, new);
     subnum++;
+#if HAVE_HS
     ses->subs_dirty=true;
+#endif
     if (ses->mesvar[MSG_SUBSTITUTE])
     {
         if (strcmp(right, EMPTY_LINE))
@@ -134,7 +136,9 @@ static void unsub(const char *arg, bool gag, struct session *ses)
         tintin_printf(ses, "#THAT SUBSTITUTE (%s) IS NOT DEFINED.", left);
     }
 
+#ifdef HAVE_HS
     ses->subs_dirty=true;
+#endif
 }
 
 void unsubstitute_command(const char *arg, struct session *ses)
