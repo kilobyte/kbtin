b98f14d7c78957759320267a19f95f10bb20b20b
Adam Borowski <kilobyte@angband.pl>
2026-07-07 16:02:04 +0200

    Don't use typecast code pointers, C++ hates that.
    Except for things that'd abort anyway.

--- user_pipe.cc
+++ user_pipe.cc
@@ -97,6 +97,10 @@ static void user_noop(void)
 {
 }
 
+static void user_noop_textout_draft(const char*, bool)
+{
+}
+
 void userpipe_initdriver(void)
 {
     ui_sep_input=false;
@@ -110,8 +114,8 @@ void userpipe_initdriver(void)
     user_pause          = user_noop;
     user_resume         = user_noop;
     user_textout        = userpipe_textout;
-    user_textout_draft  = (void (*)(const char*, bool))user_noop;
-    user_strenghten_draft= (void (*)(void))user_noop;
+    user_textout_draft  = user_noop_textout_draft;
+    user_strenghten_draft= user_noop;
     user_process_kbd    = userpipe_process_kbd;
     user_beep           = userpipe_beep;
     user_keypad         = (void (*)(bool))user_illegal;
